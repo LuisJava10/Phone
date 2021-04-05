24f3dd181d2892e4c2245cca09a1c4b7d138c6a4
Julian mosquera <arestrepo397@outlook.com>
2021-04-05 14:49:20 -0500

    Se creo la clase Dao Servicio

---
+++ src/dao/DATOTecnico.java
@@ -0,0 +1,42 @@
+
+package dao;
+
+import dto.DTOArticulo;
+import dto.DTOServicioTecnico;
+
+/**
+ *
+ * @author julian
+ */
+public class DATOTecnico implements IDAO {
+
+    @Override
+    public void insertar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object consultar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object consultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+     //Cada uno de estos metodos hara una instruccion SQL.
+    
+	
+	
+}

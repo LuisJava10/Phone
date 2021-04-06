184d6553d401450c180fcf6cf9524a9b566cc397
samuel <samuelbulla08@gmail.com>
2021-04-05 20:11:05 -0500

    Se empezo a crear el controlador

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import dto.DTOArticulo;
+
+/**
+ *
+ * @author Samuel
+ */
+
+    public class Controlador {
+    
+    /*private Principal vista;
+    private DTOArticulo producto;
+    
+    public Controlador(Principal vista, DTOArticulo producto) {
+        this.vista = vista;
+        this.producto = producto;
+       
+    }
+    
+     public void iniciar(){
+        
+        vista.setTitle("Phone Store");
+        vista.setLocationRelativeTo(null);
+        
+    }
+
+    */
+    
+}
